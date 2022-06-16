027313cca98672c72761f0196d18e0cfe384fcb2
Sadam452 <hussainganie388@gmail.com>
2022-06-16 22:59:16 +0530

    -> Established database connection "firebase" -> User can now search for any mosque timings without setting favourite mosque

--- Alwaqt/app/src/main/java/com/example/alwaqt/MainActivity4.java
+++ Alwaqt/app/src/main/java/com/example/alwaqt/MainActivity4.java
@@ -1,14 +1,86 @@
 package com.example.alwaqt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    EditText getID;
+    Button search;
+    RecyclerView recyclerView;
+    MainAdapter mainAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        getID = findViewById(R.id.getID);
+        search = findViewById(R.id.search);
+
+
+//        mosqueName = findViewById(R.id.mosqueName);
+//        namaaz1 = findViewById(R.id.namaaz1);
+//        namaaz2 = findViewById(R.id.namaaz2);
+//        namaaz3 = findViewById(R.id.namaaz3);
+//        namaaz4 = findViewById(R.id.namaaz4);
+//        namaaz5 = findViewById(R.id.namaaz5);
+//        mosqueName.setVisibility(View.GONE);
+//        namaaz1.setVisibility(View.GONE);
+//        namaaz2.setVisibility(View.GONE);
+//        namaaz3.setVisibility(View.GONE);
+//        namaaz4.setVisibility(View.GONE);
+//        namaaz5.setVisibility(View.GONE);
+//        mosqueName.setEnabled(false);
+//        namaaz1.setEnabled(false);namaaz2.setEnabled(false);namaaz3.setEnabled(false);namaaz4.setEnabled(false);namaaz5.setEnabled(false);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getID.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity4.this, "Please fill the Mosque ID field", Toast.LENGTH_SHORT).show();
+                }
+                else{
+//                    mosqueName.setVisibility(View.VISIBLE);
+//                    namaaz1.setVisibility(View.VISIBLE);
+//                    namaaz2.setVisibility(View.VISIBLE);
+//                    namaaz3.setVisibility(View.VISIBLE);
+//                    namaaz4.setVisibility(View.VISIBLE);
+//                    namaaz5.setVisibility(View.VISIBLE);
+                    //@Override
+                    //protected void onStart() {
+                     //   super.onStart();
+                    FirebaseRecyclerOptions<MainModel> options =
+                            new FirebaseRecyclerOptions.Builder<MainModel>()
+                                    .setQuery(FirebaseDatabase.getInstance().getReference().child("mosque").orderByChild("mosqueID").equalTo(getID.getText().toString()),MainModel.class)
+                                    .build();
+                    mainAdapter = new MainAdapter(options);
+                    recyclerView.setAdapter(mainAdapter);
+                    mainAdapter.startListening();
+
+                    //}
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
     }
 }
---
+++ Alwaqt/app/src/main/java/com/example/alwaqt/MainModel.java
@@ -0,0 +1,146 @@
+package com.example.alwaqt;
+
+public class MainModel {
+    String asrAzaan,asrNamaaz,fajrAzaan,fajrNamaaz,ishaAzaan,ishaNamaaz,
+            jummah,location,maghribAzaan,maghribNamaaz,mosqueName,sunrise,sunset,zuharAzaan,zuharNamaaz;
+    MainModel(){
+        //default constructor, do's nothing
+    }
+    public MainModel(String asrAzaan, String asrNamaaz, String fajrAzaan, String fajrNamaaz, String ishaAzaan, String ishaNamaaz, String jummah, String location, String maghribAzaan, String maghribNamaaz, String mosqueName, String sunrise, String sunset, String zuharAzaan, String zuharNamaaz) {
+        this.asrAzaan = asrAzaan;
+        this.asrNamaaz = asrNamaaz;
+        this.fajrAzaan = fajrAzaan;
+        this.fajrNamaaz = fajrNamaaz;
+        this.ishaAzaan = ishaAzaan;
+        this.ishaNamaaz = ishaNamaaz;
+        this.jummah = jummah;
+        this.location = location;
+        this.maghribAzaan = maghribAzaan;
+        this.maghribNamaaz = maghribNamaaz;
+        this.mosqueName = mosqueName;
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+        this.zuharAzaan = zuharAzaan;
+        this.zuharNamaaz = zuharNamaaz;
+    }
+
+    public String getAsrAzaan() {
+        return asrAzaan;
+    }
+
+    public void setAsrAzaan(String asrAzaan) {
+        this.asrAzaan = asrAzaan;
+    }
+
+    public String getAsrNamaaz() {
+        return asrNamaaz;
+    }
+
+    public void setAsrNamaaz(String asrNamaaz) {
+        this.asrNamaaz = asrNamaaz;
+    }
+
+    public String getFajrAzaan() {
+        return fajrAzaan;
+    }
+
+    public void setFajrAzaan(String fajrAzaan) {
+        this.fajrAzaan = fajrAzaan;
+    }
+
+    public String getFajrNamaaz() {
+        return fajrNamaaz;
+    }
+
+    public void setFajrNamaaz(String fajrNamaaz) {
+        this.fajrNamaaz = fajrNamaaz;
+    }
+
+    public String getIshaAzaan() {
+        return ishaAzaan;
+    }
+
+    public void setIshaAzaan(String ishaAzaan) {
+        this.ishaAzaan = ishaAzaan;
+    }
+
+    public String getIshaNamaaz() {
+        return ishaNamaaz;
+    }
+
+    public void setIshaNamaaz(String ishaNamaaz) {
+        this.ishaNamaaz = ishaNamaaz;
+    }
+
+    public String getJummah() {
+        return jummah;
+    }
+
+    public void setJummah(String jummah) {
+        this.jummah = jummah;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMaghribAzaan() {
+        return maghribAzaan;
+    }
+
+    public void setMaghribAzaan(String maghribAzaan) {
+        this.maghribAzaan = maghribAzaan;
+    }
+
+    public String getMaghribNamaaz() {
+        return maghribNamaaz;
+    }
+
+    public void setMaghribNamaaz(String maghribNamaaz) {
+        this.maghribNamaaz = maghribNamaaz;
+    }
+
+    public String getMosqueName() {
+        return mosqueName;
+    }
+
+    public void setMosqueName(String mosqueName) {
+        this.mosqueName = mosqueName;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+
+    public String getZuharAzaan() {
+        return zuharAzaan;
+    }
+
+    public void setZuharAzaan(String zuharAzaan) {
+        this.zuharAzaan = zuharAzaan;
+    }
+
+    public String getZuharNamaaz() {
+        return zuharNamaaz;
+    }
+
+    public void setZuharNamaaz(String zuharNamaaz) {
+        this.zuharNamaaz = zuharNamaaz;
+    }
+}
